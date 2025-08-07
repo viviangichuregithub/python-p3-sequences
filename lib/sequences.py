fa4ddd4ae3d9ab35992f3c787728bc587b53f5d5
Vivian Gichure <viviangichure@gmail.com>
2025-08-07 10:14:01 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < length:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+
+    print(fib_sequence)
+
